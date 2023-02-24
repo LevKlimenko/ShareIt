734b85d29d3ea78586af225addd2433242812627
Лев Клименко <klimenko.lev@yandex.ru>
2023-02-24 11:38:01 +0300

    add gateway and server folders

---
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -0,0 +1,56 @@
+package ru.practicum.shareit.booking;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.dto.BookingIncomingDto;
+import ru.practicum.shareit.booking.dto.BookingState;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+
+@Controller
+@RequiredArgsConstructor
+@Validated
+@RequestMapping(path = "/bookings")
+public class BookingController {
+    private final BookingClient bookingClient;
+
+    @PostMapping
+    public ResponseEntity<Object> saveBooking(@RequestHeader("X-Sharer-User-Id") Long userId, @Valid @RequestBody BookingIncomingDto bookingInDto) {
+        return bookingClient.saveBooking(userId, bookingInDto);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public ResponseEntity<Object> approveBoking(@RequestHeader("X-Sharer-User-Id") Long userId, @PathVariable Long bookingId,
+                                                @RequestParam("approved") Boolean approved) {
+        return bookingClient.approveBooking(userId, bookingId, approved);
+    }
+
+    @GetMapping("/{bookingId")
+    public ResponseEntity<Object> findById(@RequestHeader("X-Sharer-User-Id") Long userId, @PathVariable Long bookingId) {
+        return bookingClient.getById(userId, bookingId);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> findAllByBooker(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                  @RequestParam(required = false, defaultValue = "ALL") String stateBooking,
+                                                  @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+                                                  @RequestParam(defaultValue = "20") @Positive int size) {
+        BookingState state = BookingState.from(stateBooking);
+        return bookingClient.getAllByBooker(userId, state, from, size);
+    }
+
+    @GetMapping("/owner")
+    public ResponseEntity<Object> findAllByOwner(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                 @RequestParam(required = false, defaultValue = "ALL") String stateBooking,
+                                                 @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+                                                 @RequestParam(defaultValue = "20") @Positive int size) {
+        BookingState state = BookingState.from(stateBooking);
+        return bookingClient.getAllByBooker(userId, state, from, size);
+    }
+
+}
